fd5ecfa328aa7337a332cb23682620dcad8603ab
Mahalakshmi <122530279+mahasiva123456789@users.noreply.github.com>
2025-05-03 23:37:11 +0530

    Update Account.jsx

--- src/Components/Account.jsx
+++ src/Components/Account.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Account.css'; 
 
 const Account = () => {
-    const navigate = useNavigate()
-    const handlesignout=()=>{
-        localStorage.removeItem('token');
-        navigate('/login')
+  const navigate = useNavigate();
 
-    }
-    const handleloginout=()=>{
-      navigate('/login')
-    }
+  const handlesignout = () => {
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
+
+  const handleloginout = () => {
+    navigate('/login');
+  };
+
   return (
-    <>
-    <div>Account</div>
-    <button onClick={handlesignout} style={{marginRight:"10px"}}>SIGNOUT</button>
-    <button onClick={handleloginout}>login</button>
-    </>
-  )
-}
+    <div className="account-container">
+      <div className="account-title">Account</div>
+      <div className="account-buttons">
+        <button onClick={handlesignout} className='signbtn'>SIGNOUT</button>
+        <button onClick={handleloginout} className='loginbtn'>LOGIN</button>
+      </div>
+    </div>
+  );
+};
 
-export default Account
+export default Account;
