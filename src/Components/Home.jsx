98f4ed21cd2a6bbac3c9686c2e04ad120da3372c
Mahalakshmi <122530279+mahasiva123456789@users.noreply.github.com>
2025-05-03 22:50:22 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -19,16 +19,6 @@ const Home = () => {
          <p>Essential Furniture for Every Room</p>
      </div>
      <Link to='/product' style={{textDecoration:"none"}}> <div className='catagorycon'>
-
-        <img src="https://www.woodenstreet.com/images/home-new1/icon1.jpg" alt="" />
-        <img src="https://www.woodenstreet.com/images/home-new1/icon3.jpg" alt="" />
-        <img src="https://www.woodenstreet.com/images/home-new1/icon4.jpg" alt="" />
-        <img src="https://www.woodenstreet.com/images/home-new1/icon8.jpg" alt="" />
-        <img src="https://www.woodenstreet.com/images/home-new1/icon7.jpg" alt="" />
-        <img src="https://www.woodenstreet.com/images/home-new1/icon10.jpg" alt="" />
-        <img src="https://www.woodenstreet.com/images/home-new1/icon8.jpg" alt="" />
-        <img src="https://www.woodenstreet.com/images/home-new1/icon6.jpg" alt="" />
-        <img src="https://www.woodenstreet.com/images/home-new1/icon11.jpg" alt="" />
          <img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746281863/icon9_vuczlk.jpg" alt="" />
          <img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746281858/icon11_d9gktl.jpg" alt="" />
          <img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746281847/icon12_t0z64t.jpg" alt="" />
@@ -49,15 +39,6 @@ const Home = () => {
      </div>
      <div className='furniturebrand_top'>
 
-      <div className='brandimg'><img src="	https://www.woodenstreet.com/images/home-new1/space-save-banner.jpg?v=3011" alt="" /></div>
-       <div className='brandimg'><img src="	https://www.woodenstreet.com/images/home-new1/beds-banner.jpg?v=301" alt="" /></div>
-      <div className='brandimg'><img src="https://www.woodenstreet.com/images/home-new1/dining-set-banner.jpg?v=301" alt="" /></div>
-     </div>
-     <div className='furniturebrand_top'>
-       <div className='brandimg'><img src="	https://www.woodenstreet.com/images/home-new1/wfh-banner.jpg?v=301" alt="" /></div>
-       <div className='brandimg'><img src="		https://www.woodenstreet.com/images/home-new1/outdoor-banner.jpg?v=3011" alt="" /></div>
-       <div className='brandimg'><img src="https://www.woodenstreet.com/images/home-new1/kids-room-banner.jpg?v=3011" alt="" /></div>
-
       <div className='brandimg'><img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746281125/beds-banner_uimuio.jpg" alt="" /></div>
        <div className='brandimg'><img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746281323/outdoor-banner_desghj.jpg" alt="" /></div>
       <div className='brandimg'><img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746281356/space-save-banner_zcwcmj.jpg" alt="" /></div>
@@ -74,9 +55,6 @@ const Home = () => {
         <div className='aboutcon_left'>
         <div className='abouthead'><h1>ABOUT US</h1></div>
          <div className='aboutimg'>
-
-         <img src="	https://furnituretrader.com.au/media/wysiwyg/HomePageImages/AnimatedCollectionsImageDesktop.gif" alt="" />
-
          <img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746282192/AnimatedCollectionsImageDesktop_c4xs8q.gif" alt="" />
 
          </div>
@@ -94,34 +72,15 @@ const Home = () => {
   <div className='furniture_quotes'>
          <h1>Reviews</h1>
          <p>EDiscover what our customers are saying!</p>
-     </div>
+  </div>
     {/* products gif category */}
     <div className='furniturebrand_top'>
-
-      <div className='brandimg1'><img src="	https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYm5rZDF3OHUzeTdmMG55d3NjajloamhoeXR1dzZnNDZ2ZGkzdHVidyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l41lHnG1PgQiniysg/giphy.webp" alt="" /></div>
-       <div className='brandimg1'><img src="	https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExczN4ZWxocDAxMHl3eW8wc3JpYWVkZW5iaHkwdHV1aHpwdG1vZTdnOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Ymtw5fjm9Nl01J2aqC/giphy.webp" alt="" /></div>
-      <div className='brandimg1'><img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2dwOG96aHIwc255bWphbjVlYWdjN3N6aW1uY3VlZzBwaTlvZnpudSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YvQyE42IvYTS/giphy.webp" alt="" /></div>
-      <div className='brandimg1'><img src="https://media2.giphy.com/media/XJa7zyWdNHGFTBDfZU/200.webp?cid=ecf05e47rpxqcbqamhjh13rlm959n7ufsuo5weuc0kzxejfr&ep=v1_gifs_related&rid=200.webp&ct=g" alt="" /></div>
-     </div>
-
-     <div className='reviewcon'>
-      <img src="https://images.woodenstreet.de/image/cache/data%2F16653940551656867703.IMG-20220703-WA0126+%282%29-max-337.jpg" alt="" />
-      <img src="https://m.media-amazon.com/images/I/7151KKXLCrL._SL1600_.jpg" alt="" />
-      <img src="https://m.media-amazon.com/images/I/61Eoj-aagSL._SL1600_.jpg" alt="" />
-      <img src="https://m.media-amazon.com/images/I/716KM++Rz4L.jpg" alt="" />
-      <img src="https://m.media-amazon.com/images/I/71Sv+2y2JeL.jpg" alt="" />
-      <img src="https://m.media-amazon.com/images/I/61se4rskppL.jpg" alt="" />
-      <img src="https://m.media-amazon.com/images/I/718Lls2FfbL.jpg" alt="" />
-      <img src="https://m.media-amazon.com/images/I/61Eoj-aagSL._SL1600_.jpg" alt="" />
-      <img src="https://m.media-amazon.com/images/I/716KM++Rz4L.jpg" alt="" />
-      <img src="https://m.media-amazon.com/images/I/71Sv+2y2JeL.jpg" alt="" />
-      <img src="https://m.media-amazon.com/images/I/61se4rskppL.jpg" alt="" />
-
-      <div className='brandimg1'><img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746282785/giphy_fltyub.webp" alt="" /></div>
+     <div className='brandimg1'><img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746282785/giphy_fltyub.webp" alt="" /></div>
        <div className='brandimg1'><img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746282865/giphy_f6ngqy.webp" alt="" /></div>
       <div className='brandimg1'><img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746282906/giphy_oqg88y.webp" alt="" /></div>
       <div className='brandimg1'><img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746282933/200.webp_qwivfk.webp" alt="" /></div>
      </div>
+
 
      <div className='reviewcon'>
       <img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746283089/7151KKXLCrL._SL1600__ttshse.jpg" alt="" />
@@ -135,8 +94,6 @@ const Home = () => {
       <img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746283424/716KM_Rz4L_kni6o5.jpg" alt="" />
       <img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746283474/71Sv_2y2JeL_jzynql.jpg" alt="" />
       <img src="https://res.cloudinary.com/dpdwvkw0m/image/upload/v1746283496/61se4rskppL_vnq4zg.jpg" alt="" />
-
-
      </div>
 
    <Footer />
